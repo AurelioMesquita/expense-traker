bf653e919cb1750a578efb5b203316cc9bd05935
Aurélio Nogueira Mesquita <aurelio.mesquita@interaduaneira.com.br>
2022-09-08 21:29:53 -0300

    criando projeto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as C from "./App.styles";
+function App() {
+  return (
+    <>
+      <C.Container>
+        <C.Header>
+          <C.HeaderText>Sistema Financeiro</C.HeaderText>
+        </C.Header>
+        <C.Body></C.Body>
+      </C.Container>
+    </>
+  );
+}
+
+export default App;
